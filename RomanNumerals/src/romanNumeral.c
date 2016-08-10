dec833fe2fbf62876176b002ef2a0901d72b031c
tburch <tburch@tburch-Satellite-L655>
2016-08-09 23:14:17 -0400

    Added error codes/descriptions and return codes.

--- RomanNumerals/src/romanNumeral.c
+++ RomanNumerals/src/romanNumeral.c
@@ -8,6 +8,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Return code strings
+static const char* retDesc[] =
+{
+		"Success",
+		"Could not determine length of input string.",
+		"Numeral does not contain a valid Roman Numeral.",
+		"Roman numeral specified is greater than 3999.",
+		"Difference is less than 0."
+		"Numeral is null."
+};
+
+const char* getRetCodeDesc(NumeralReturnCode retCode)
+{
+	return retDesc[retCode];
+}
+
 Numeral* numeral_new()
 {
 	Numeral* aNumeral = (Numeral*) malloc(sizeof(Numeral));
@@ -19,17 +35,62 @@ Numeral* numeral_new()
 	return aNumeral;
 }
 
-int add(char* firstNumber, char* secondNumber)
+static int IsPointerValid(Numeral* numeralToCheck)
 {
-	// protect against char buffer overrun
+	int returnCode = EXIT_SUCCESS;
+	if (NULL == numeralToCheck)
+	{
+		returnCode = EXIT_FAILURE;
+	}
 
-	// verify that both numbers are valid Roman numerals
-
-	// verify that we don't exceed the max number (3999)
-	return 0;
+	return returnCode;
+}
+static int IsStringLenValid(Numeral* numeralToCheck)
+{
+	int returnCode = EXIT_SUCCESS;
+	if (strlen(numeralToCheck->text) <=0 )
+	{
+		returnCode = EXIT_FAILURE;
+	}
+	return returnCode;
 }
 
-int subtract (char* firstNumber, char* secondNumber)
+//static int ValidateText(Numeral* numeralToCheck)
+//{
+//
+//	return 0;
+//}
+
+NumeralReturnCode add(Numeral* first, Numeral* second, Numeral* answer)
+{
+	int returnCode = EXIT_SUCCESS;
+	// Validation
+	{
+		// Verify that all pointers are not null
+		if (!(IsPointerValid(first) &&
+				IsPointerValid(second) &&
+				IsPointerValid(answer)))
+		{
+			returnCode = INVALID_NUMERAL_PTR;
+		}
+
+		// Verify that the string length of the first and second arguments are OK
+		if (returnCode == EXIT_SUCCESS &&
+				!(IsStringLenValid(first) && IsStringLenValid(second)))
+		{
+			returnCode = INVALID_NUMERAL_PTR;
+		}
+
+		// verify that both numbers are valid Roman numerals
+
+	}
+	// Convert the Numeral to unsigned integer
+
+	// verify that we don't exceed the max number (3999)
+	return returnCode;
+}
+
+ NumeralReturnCode subtract(Numeral* first, Numeral* second, Numeral* difference)
 {
 	// verify that the first number is greater than the second number
 
--- RomanNumerals/src/romanNumeral.h
+++ RomanNumerals/src/romanNumeral.h
@@ -9,17 +9,32 @@
 #define ROMANNUMERAL_H_
 
 #define MAX_WIDTH  50
+#define ERR_WIDTH  25
+#define NUM_ERRORS 4
 
-// base structure for
-// all parts of the Numeral
-// with number equivalents
+// Type for all parts of the Numeral
+// with unsigned integer equivalents
 typedef struct {
 	char text[MAX_WIDTH];
 	unsigned value;
 } Numeral;
 
+// Errors
+typedef enum returnCodes {
+	NON_NULL_TERM_STRING = -1,
+	INVALID_TEXT = -2,
+	VALUE_OVERFLOW = -3,
+	NEGATIVE_VALUE = -4,
+	INVALID_NUMERAL_PTR = -5
+} NumeralReturnCode;
+
+const char* getRetCodeDesc(NumeralReturnCode retCode);
+
+// Constructor
 Numeral* numeral_new();
 
+NumeralReturnCode add(Numeral* first, Numeral* second, Numeral* sum);
+NumeralReturnCode subtract(Numeral* first, Numeral* second, Numeral* difference);
 
 
 #endif /* ROMANNUMERAL_H_ */
